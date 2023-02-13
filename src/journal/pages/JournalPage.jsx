141ee1f139d86dfdc6055f5ff78ae9c9ac872466
CherokiDev <miguelangelmorato84@gmail.com>
2023-02-13 18:18:43 +0100

    created NavBar, SideBar and JournalLayout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,13 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
-  return <Typography variant="h1">JournalPage</Typography>;
+  return (
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quia
+        voluptates, quod, voluptatibus, voluptas quae quos voluptatum
+      </Typography>
+    </JournalLayout>
+  );
 };
